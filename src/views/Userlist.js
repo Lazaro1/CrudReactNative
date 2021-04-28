effc234c43699be92abcd890c41c189bacdf4f07
LAZARO <lazarolimasilva2@gmail.com>
2021-04-27 23:41:42 -0300

    refact tomorrow sintax, she is not function

--- src/views/Userlist.js
+++ src/views/Userlist.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { View, Text, FlatList } from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
+import { ListItem, Avatar, Button, Icon } from 'react-native-elements'
 
 import users from '../data/users'
 
 export default props => {
+    
+    function getActions(user) {
+        return(
+            <>
+                <Button 
+                    onPress={() => props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange"/>}
+                />
+            </>
+        )
+    }
 
     function getUserItem({ item: user }) {
         return (
-            <ListItem bottomDivider >
+            <ListItem bottomDivider rightElement={getActions(user)}>
                 <Avatar source={{ uri: user.avatarUrl }} onPress={() => props.navigation.navigate('UserForm')} />
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
